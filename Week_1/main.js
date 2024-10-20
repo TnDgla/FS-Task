6f2b7b2c974c1ac097ce3b44f4bcd7a4c1a42ab5
Piyush Gupta <122008271+PIYUSH-0612@users.noreply.github.com>
2024-10-20 13:40:12 +0530

    Add files via upload

---
+++ Week_1/main.js
@@ -0,0 +1,28 @@
+
+const subjectDetails = {
+    "Maths": "Mr. Arvind Agarrwal [Room no. 202] ",
+    "Science": "Mr. Asheshanand Acharaya [Room no. 300]",
+    "Hindi": "Ms. Pushpanjali [Room no. 101]",
+    "English": "Ms. Shazda Ali [Room no. 401]",
+    "Game": "Ms. Sulekha [Sports room]",
+    "Social": "Mr. R.D [Room no. 209]",
+    "Arts": "Ms. Neelam [Art room]"
+
+};
+
+
+function showDetails(subject) {
+    const details = subjectDetails[subject] || "Details not available.";
+    alert(`You clicked on: ${subject}\n${details}`);
+}
+
+function applyHoverEffect() {
+    const links = document.querySelectorAll('.timetable a');
+
+    links.forEach(link => {
+        const subject = link.textContent.trim();
+        link.classList.add(`hover-${subject}`);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', applyHoverEffect);
